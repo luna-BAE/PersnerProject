c2855e4d7fedbe8c3d16d7388ddb8e24286f349f
luna <dichotomy.bgm@gmail.com>
2023-03-28 12:15:16 +0900

    메인 페이지 디자인 작업중

---
+++ homepage/src/main/java/com/luna/homepage/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.luna.homepage.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.luna.homepage.model.service.GuestbookService;
+import com.luna.homepage.model.vo.GuestbookVo;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	GuestbookService guestbookService;
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String index() {
+		
+		return "home";
+	}
+	
+}
---
+++ homepage/src/main/webapp/WEB-INF/views/guestbook/guestbookList.jsp
@@ -0,0 +1,7 @@
+<%@ include file = "layout/header.jspf" %>
+
+<title>Insert title here</title>
+</head>
+<body>
+
+<%@ include file = "layout/footer.jspf" %>
